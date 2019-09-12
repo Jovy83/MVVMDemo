26cbac6eb545f1f0da344fa19e81f535fd49deef
Jovy83 <jovy83@live.com>
2019-09-11 20:09:23 -0700

    add binding examples

--- WPFDemo/ViewModels/ShellViewModel.cs
+++ WPFDemo/ViewModels/ShellViewModel.cs
@@ -9,5 +9,36 @@ namespace WPFDemo.ViewModels {
     // Screen allows more control over opening and closing it. we get events when the user closes the app. we can use to display save prompts (you have unsaved changed, do you want to save before closing?).
     public class ShellViewModel : Screen {
 
+        private string _firstName = "JOVY";
+
+        public string FirstName {
+            get {
+                return _firstName;
+            }
+            set {
+                _firstName = value;
+                NotifyOfPropertyChange(() => FirstName); // notify anyone who want to listen that the FirstName property has changed
+                NotifyOfPropertyChange(() => FullName); // whenever first name is changed, the full name is also changed. so notify property change of FullName 
+            }
+        }
+
+        private string _lastName;
+
+        public string LastName {
+            get { return _lastName; }
+            set {
+                _lastName = value;
+                NotifyOfPropertyChange(() => LastName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+
+        public string FullName {
+            get {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
     }
 }
