cd7a35b7de116b519933fa88e3563fe5682c0331
Jovy83 <jovy83@live.com>
2019-09-11 19:42:55 -0700

    Add project files.

---
+++ WPFDemo/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPFDemo.ViewModels;
+
+namespace WPFDemo {
+    // starting point for our application with caliburn micro mvvm
+    public class Bootstrapper : BootstrapperBase {
+        public Bootstrapper() {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e) {
+            DisplayRootViewFor<ShellViewModel>(); // shellviewmodel is like the base/standard viewmodel. it's like mainwindow.xaml is for the default template
+
+            // we're basically saying: this is what we want to launch which is the ShellVM
+        }
+
+    }
+}
---
+++ WPFDemo/ViewModels/ShellViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDemo.ViewModels {
+    // Screen allows more control over opening and closing it. we get events when the user closes the app. we can use to display save prompts (you have unsaved changed, do you want to save before closing?).
+    public class ShellViewModel : Screen {
+
+    }
+}
