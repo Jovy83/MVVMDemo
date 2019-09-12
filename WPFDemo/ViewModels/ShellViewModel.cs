0e638da01a91e5da13f5ad915f5ea8bf8d8a7521
Jovy83 <jovy83@live.com>
2019-09-11 20:39:40 -0700

    add combobox binding example

--- WPFDemo/ViewModels/ShellViewModel.cs
+++ WPFDemo/ViewModels/ShellViewModel.cs
@@ -4,12 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPFDemo.Models;
 
 namespace WPFDemo.ViewModels {
     // Screen allows more control over opening and closing it. we get events when the user closes the app. we can use to display save prompts (you have unsaved changed, do you want to save before closing?).
     public class ShellViewModel : Screen {
 
         private string _firstName = "JOVY";
+        private string _lastName;
+        private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
+        private PersonModel _personModel;
+
+
+        public ShellViewModel() {
+            People.Add(new PersonModel { FirstName = "Tim", LastName = "Corey" });
+            People.Add(new PersonModel { FirstName = "Bill", LastName = "Jones" });
+            People.Add(new PersonModel { FirstName = "Sue", LastName = "Storm" });
+        }
 
         public string FirstName {
             get {
@@ -21,9 +32,6 @@ namespace WPFDemo.ViewModels {
                 NotifyOfPropertyChange(() => FullName); // whenever first name is changed, the full name is also changed. so notify property change of FullName 
             }
         }
-
-        private string _lastName;
-
         public string LastName {
             get { return _lastName; }
             set {
@@ -32,11 +40,24 @@ namespace WPFDemo.ViewModels {
                 NotifyOfPropertyChange(() => FullName);
             }
         }
-
-
         public string FullName {
             get {
                 return $"{FirstName} {LastName}";
+            }
+        }
+
+
+        public BindableCollection<PersonModel> People {
+            get { return _people; }
+            set { _people = value; }
+        }
+
+
+        public PersonModel SelectedPerson {
+            get { return _personModel; }
+            set {
+                _personModel = value;
+                NotifyOfPropertyChange(() => SelectedPerson);
             }
         }
 
